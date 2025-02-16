b51b98c0347c429229daf4faec165ad3ee963bad
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-02-16 14:19:25 +0200

    P01|Q01 - Find all products that are out of stock.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Assignment
+﻿using static Assignment.ListGenerator;
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01 - Restriction Operators
+
+            #region P01|Q01 - Find all products that are out of stock.
+
+            #region 01 - Fluent Syntax
+
+            //var result = ProductsList.Where(p => p.UnitsInStock == 0);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine();
+
+            #endregion
+
+            #region 02 - Query Syntax
+
+            //var result = from product in ProductsList
+            //             where product.UnitsInStock == 0
+            //             select product;
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine();
+
+            #endregion
+
+            #endregion
+
+            #endregion
         }
     }
 }
